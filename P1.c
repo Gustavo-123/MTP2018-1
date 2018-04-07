3a25449d8ac3afe4a795ad263c3d3754b10d5cfd
Gustavo Castro Souza <37852565+Gustavo-123@users.noreply.github.com>
2018-04-07 10:08:17 -0300

    Create P1.c

---
+++ P1.c
@@ -0,0 +1,31 @@
+/* Aluno: Gustavo Castro Souza
+Matricula: 11811EEL037 */
+
+#include<stdio.h>
+
+int main(){
+	
+	int estado=0, i=0;
+	char bits[256];
+	
+	scanf("%s", bits);
+	
+	while(bits[i] != '\0') {
+		
+		if(bits[i]=='0'&&estado==0||bits[i]=='1'&&estado==1)
+			estado=0;
+		else if(bits[i]=='1'&&estado==0||bits[i]=='0'&&estado==2)
+			estado=1;
+		else if(bits[i]=='0'&&estado==1||bits[i]=='1'&&estado==2)
+			estado=2;
+		
+		i++;
+	}
+
+	if(estado==0)
+		printf("%s e multiplo de 3\n", bits);
+	else
+		printf("%s nao e\n", bits);
+	
+	return 0;
+}
